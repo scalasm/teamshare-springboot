6412dda026762075a83010fceb578134ea371848
Mario Scalas <mario.scalas@linksmt.it>
2019-01-21 13:34:04 +0100

    Aggiunto il modulo di business logic

---
+++ business/src/main/java/it/linksmt/teamshare/business/config/AppConfig.java
@@ -0,0 +1,11 @@
+package it.linksmt.teamshare.business.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// TODO Questo è inutile con Spring Boot: rimuovere 'sta classe!
+//@Configuration
+//@ComponentScan("it.linksmt.teamshare.business.services")
+public class AppConfig {
+
+}
