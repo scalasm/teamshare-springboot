12caa207e7c1bc7ec958addbc2062a426ec1c887
Mario Scalas <mario.scalas@linksmt.it>
2019-01-24 16:08:59 +0100

    API private

--- rest-api/src/main/java/it/linksmt/teamshare/rest/controllers/NoteController.java
+++ rest-api/src/main/java/it/linksmt/teamshare/rest/controllers/NoteController.java
@@ -1,6 +1,6 @@
-	package it.linksmt.teamshare.rest.controllers;
+package it.linksmt.teamshare.rest.controllers;
 
-	import java.util.List;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,66 +24,59 @@ import it.linksmt.teamshare.business.dtos.NoteDto;
 import it.linksmt.teamshare.business.request.NoteRequestDto;
 import it.linksmt.teamshare.business.services.NoteService;
 
-
-@Api(value = "Note Controller", description = "Note Controller", tags = { "Note" })
+@Api( value = "Note Controller", description = "Note Controller", tags = { "Note" } )
 @Controller
-@RequestMapping(name = "Note Controller",value = "/note", produces = { MediaType.APPLICATION_JSON_VALUE })
-
+@RequestMapping( name = "Note Controller", value = "/private/note", produces = { MediaType.APPLICATION_JSON_VALUE } )
 public class NoteController {
 
+	@Autowired
+	private NoteService noteService;
 
+	/**
+	 * @RequestParam(name = "numero_pagina", required = false, defaultValue =
+	 *                    "0")
+	 * @ApiParam(value = "Numero di pagina", required = false, defaultValue =
+	 *                 "0") final int numeroPagina,
+	 * @RequestParam(name = "numero_elementi_per_pagina", required = false,
+	 *                    defaultValue = "0")
+	 * @ApiParam(value = "Numero di elementi per pagina", required = false,
+	 *                 defaultValue = "0") final int numeroElementiPerPagina
+	 * @return
+	 */
 
-	
-		
-		@Autowired
-		private NoteService noteService;
-
-		
-		/**
-		 * @RequestParam(name = "numero_pagina", required = false, defaultValue = "0")
-		 *  @ApiParam(value = "Numero di pagina", required = false, defaultValue = "0") final int numeroPagina,
-			@RequestParam(name = "numero_elementi_per_pagina", required = false, defaultValue = "0")
-			@ApiParam(value = "Numero di elementi per pagina", required = false, defaultValue = "0") final int numeroElementiPerPagina
-		 * @return
-		 */
-		
-	
-
-		@ApiOperation(value = "Note By Id", notes = "Servizio rest per visualizzare le note tramite gli id", response = NoteDto.class)
-		@ApiResponse(code = 200, message = "Note By Id", response = NoteDto.class)
-		@GetMapping( value="/")
-		public ResponseEntity<List<NoteDto>> getNote() {
-			List<NoteDto> note = noteService.getNote() ;
-			return new ResponseEntity<List<NoteDto>>(note, HttpStatus.OK);
-		}
-
-		@ApiOperation(value = "Aggiungi nota", notes = "Servizio rest per aggiungere una nota", response = NoteDto.class)
-		@ApiResponse(code = 200, message = "Nota Aggiunta", response = NoteDto.class)
-		@PostMapping( value="/")
-		public ResponseEntity<NoteDto> addUser(@RequestBody final NoteRequestDto nota ) {
-			NoteDto u = noteService.addNote(nota);
-			return new ResponseEntity<NoteDto>(u, HttpStatus.CREATED);
-		}
-		
-		@ApiOperation(value = "Update nota", notes = "Servizio rest per aggiornare una nota", response = NoteDto.class)
-		@ApiResponse(code = 200, message = "Update nota", response = NoteDto.class)
-		@PutMapping( value="/{noteId}")
-		public ResponseEntity<NoteDto> updateUser( @RequestParam("noteId") Integer idNota, @RequestBody final NoteRequestDto nota) {
-			NoteDto u = noteService.updateNote(idNota,nota);
-			return new ResponseEntity<NoteDto>(u, HttpStatus.OK);			
-		}
-
-		@ApiOperation(value = "Delete nota", notes = "Servizio rest per cancellare una nota")
-		@ApiResponse(code = 200, message = "Delete nota")
-		@DeleteMapping( value="/{noteid}")
-		public void removeUser(@RequestParam(name = "noteId", required = true) @ApiParam(value = "noteId", required = true) final Integer noteId ) {
-			Integer id = null;
-				id = noteId;
-			if(id != null) {
-				noteService.deleteNote(id);
-			}
-		}
-
+	@ApiOperation( value = "Note By Id", notes = "Servizio rest per visualizzare le note tramite gli id", response = NoteDto.class )
+	@ApiResponse( code = 200, message = "Note By Id", response = NoteDto.class )
+	@GetMapping( value = "/" )
+	public ResponseEntity<List<NoteDto>> getNote() {
+		List<NoteDto> note = noteService.getNote();
+		return new ResponseEntity<List<NoteDto>>( note, HttpStatus.OK );
 	}
 
+	@ApiOperation( value = "Aggiungi nota", notes = "Servizio rest per aggiungere una nota", response = NoteDto.class )
+	@ApiResponse( code = 200, message = "Nota Aggiunta", response = NoteDto.class )
+	@PostMapping( value = "/" )
+	public ResponseEntity<NoteDto> addUser( @RequestBody final NoteRequestDto nota ) {
+		NoteDto u = noteService.addNote( nota );
+		return new ResponseEntity<NoteDto>( u, HttpStatus.CREATED );
+	}
 
+	@ApiOperation( value = "Update nota", notes = "Servizio rest per aggiornare una nota", response = NoteDto.class )
+	@ApiResponse( code = 200, message = "Update nota", response = NoteDto.class )
+	@PutMapping( value = "/{noteId}" )
+	public ResponseEntity<NoteDto> updateUser( @RequestParam( "noteId" ) Integer idNota, @RequestBody final NoteRequestDto nota ) {
+		NoteDto u = noteService.updateNote( idNota, nota );
+		return new ResponseEntity<NoteDto>( u, HttpStatus.OK );
+	}
+
+	@ApiOperation( value = "Delete nota", notes = "Servizio rest per cancellare una nota" )
+	@ApiResponse( code = 200, message = "Delete nota" )
+	@DeleteMapping( value = "/{noteid}" )
+	public void removeUser( @RequestParam( name = "noteId", required = true ) @ApiParam( value = "noteId", required = true ) final Integer noteId ) {
+		Integer id = null;
+		id = noteId;
+		if (id != null) {
+			noteService.deleteNote( id );
+		}
+	}
+
+}
--- rest-api/src/main/java/it/linksmt/teamshare/rest/controllers/PostsController.java
+++ rest-api/src/main/java/it/linksmt/teamshare/rest/controllers/PostsController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -27,7 +29,7 @@ public class PostsController {
 
 	@ApiOperation(value = "Lista Post", notes = "Servizio rest per visualizzare tutti i post", response = PostDto.class)
 	@ApiResponse(code = 200, message = "Lista Attivita", response = PostDto.class)
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<PostDto>> getPosts() {
 		List<PostDto> posts = postService.getAll();
 		return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
@@ -35,7 +37,7 @@ public class PostsController {
 
 	@ApiOperation(value = "Aggiungere un post", notes = "Servizio rest per aggiungere un post", response = PostDto.class)
 	@ApiResponse(code = 200, message = "Aggiungere un post", response = PostDto.class)
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<PostDto> addPost(@RequestBody PostRequestDto post) {
 		PostDto p = postService.addPost(post);
 		return new ResponseEntity<PostDto>(p, HttpStatus.CREATED);
@@ -43,7 +45,7 @@ public class PostsController {
 
 	@ApiOperation(value = "Cancellare un post", notes = "Servizio rest per cancellare un post", response = String.class)
 	@ApiResponse(code = 200, message = "Cancellare un post", response = String.class)
-	@RequestMapping(value = "/{postId}", method = RequestMethod.DELETE)
+	@DeleteMapping( "/{postId}" )
 	public void removePost(@PathVariable Integer postId) {
 		if (postId != null) {
 			postService.deletePost(postId);
